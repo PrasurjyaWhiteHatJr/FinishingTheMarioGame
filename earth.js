17b0c899631e164f6f0fad037592728003cf87b3
prasurjya <65493249+PrasurjyaWhiteHatJr@users.noreply.github.com>
2020-07-27 17:52:46 +0530

    Add files via upload

---
+++ earth.js
@@ -0,0 +1,131 @@
+//bullet = createSprite(320, 500, 10, 10);
+//bullet = addImage("bullet", bimage);
+//bullet.setCollider("rectangle", 0, 0, 10, 10);
+//bullet = scale = 0.1;
+//bullet.x = mario.x;
+
+//edges = createNewEdges();
+
+//if keyDown("UP_ARROW")){
+//bullet.velocity = 10;
+//}
+
+//var = count = 0;
+
+//function draw() {
+//backGround(bgImage);
+
+//text("score: ", count, 250, 100);
+
+//if gameState===("PLAY") {
+
+  //count = count + Math.round(WorldFrameRate/60);
+//if keyDown = ("LEFT_ARROW"){
+//changePosition(-3, 0);
+//}
+
+//else if keyDown = ("RIGHT_ARROW"){
+    //changePosition(3, 0);
+//}
+
+//else if keyDown = ("UP_ARROW"){
+    //changePosition(0, -3);
+//}
+
+//else if keyDown = ("DOWN_ARROW"){
+    //changePosition(0, +3);
+//}
+
+//playSound("darwds1.mp3")
+//bullet.x = mario.x+80;
+//mario.x = mouse.x;
+
+//if (keyCode === 32) {
+  //bullet.velocityX = -10
+//}
+
+
+//bullet = createSprite(320,500,10,10);
+//bullet.addImage("bullet",bimage);
+//bullet.setCollider("rectangle",0,0,10,10);
+//bullet.scale=0.1;
+//bullet.x=mario.x;
+
+//edges=createEdgeSprites();
+
+// if(keyDown("UP_ARROW")){
+//  bullet.velocityX = 10;
+//}
+//var count=0;
+//}
+
+//function draw() {
+//background(bgimage);
+
+//text("Score: "+ count, 250, 100);
+
+//if(gameState==="play"){
+
+  //count = count + Math.round(World.frameRate/60);
+//if(keyDown(LEFT_ARROW)){ 
+ //changePosition(-3,0);
+ //} 
+//else if(keyDown(RIGHT_ARROW)){ 
+  //changePosition(3,0);
+ //} 
+ //else if(keyDown(UP_ARROW)) { 
+  //changePosition(0,-3);
+ //}
+ //else if(keyDown(DOWN_ARROW)){
+    //changePosition(0,+3);
+//} 
+//playsound("darkwds1.mp3")
+//bullet.x=mario.x+80;
+// mario.x=mouseX;
+
+
+//if (keyCode === 32) {
+ //bullet.velocityX = -10;
+ //bullet.velocityY = Math.round(random(-5,-10));
+  //shot.play();
+//}
+//if(mario.x < 750){
+  //mario.velocityX = 3;
+//}
+//if(mario.x > 50){
+ //mario.velocityX = -3
+//}
+ //if(bullet.y<650){
+//bullet.x=mario.x;
+//bullet.y=350;
+//bullet.velocityY=0;
+//}
+//}
+//if(bullet.isTouching(trex)){
+//gamestate = 'end';
+//trex.velocityY = 0;
+//trex.addImage("trexD", trexDie);
+//bullet.velocityY = 0;
+//trex.collide(edges);
+//}
+
+
+//if(gameState==="end"){
+//trex.addImage(trexDie);
+//trex.visible = false;
+//bullet.visible = false;
+//}
+
+//drawSprites();
+//createEdgeSprites();
+//trex.bounceOff(edges);
+//trex.bounceOff(ground1);
+//mario.bounceOff(edges);
+
+//mario.collide(ground2);
+
+
+//function changePosition(x, y){
+//bullet.x = bullet.x + x;
+//bullet.y = bullet.y + y;
+//}
